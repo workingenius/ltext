13bef59d66cad46ddeef174d74c05d866efb107e
ruqishang <ruqishang@linkdoc.com>
2019-06-21 15:29:01 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='ltext',
+    version='0.1.0',
+    py_modules=['ltext'],
+    author='Nathan Hardy',
+    author_email='workingenius@163.com',
+    url='https://github.com/workingenius/ltext',
+    description='A tool for annotation offset calculation',
+    license='LGPLv3'
+)
